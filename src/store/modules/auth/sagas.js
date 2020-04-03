ec442519ab5cda50bb4c63f908086cfc55b7efbb
Cassio C <LccassioL@gmail.com>
2020-04-03 15:34:25 -0300

    Implements auth system

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,26 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
 
-export default all([]);
+import history from '~/services/history';
+import api from '~/services/api';
+import { signInSuccess, signInFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'session', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/deliveries');
+  } catch (error) {
+    yield put(signInFailure());
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
